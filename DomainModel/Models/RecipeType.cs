a838a4541538d72b3a5dd10c99d829fd64eab2e3
codebeautyprogramming <codebeautyprogramming@gmail.com>
2024-04-06 15:48:25 +0200

    Startup code for this chapter

---
+++ DomainModel/Models/RecipeType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainModel.Models
+{
+    public class RecipeType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+
+        public RecipeType(string name)
+        {
+            Name = name;
+        }
+        public RecipeType() { }
+    }
+}
